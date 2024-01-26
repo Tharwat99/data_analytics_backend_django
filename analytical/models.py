b05cb778ad3c05b8fe2ca79daef1bdf571efedac
unknown <mtharwat@onesolutionc.com>
2024-01-26 15:19:16 +0200

    update Data model change month to be unique

--- analytical/models.py
+++ analytical/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Data(models.Model):
-    month = models.CharField(max_length= 100)
+    month = models.CharField(max_length= 100, unique = True)
     revenue = models.DecimalField(decimal_places= 2, max_digits=10)
     expenses = models.DecimalField(decimal_places= 2, max_digits=10)
     profit = models.DecimalField(decimal_places= 2, max_digits=10)
